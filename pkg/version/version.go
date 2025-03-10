aaf6233957ecc63e0b522a1e68898b05e5a4cb5a
Michael Curtis <michael@sailfin.io>
2025-03-10 16:21:36 -0400

    fix: try manually setting version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,5 +3,5 @@
 package version
 
 // Version holds the current version of the agent.
-// The default value is "dev" and should be overridden at build time.
-var Version = "dev"
+// The default value is "0.0.1-alpha.1" and should be overridden at build time.
+var Version = "0.0.1-alpha.1"
