1a6404fb4cbafcc0244766cd6f8c0662c369fbe3
Michael Curtis <michael@sailfin.io>
2025-03-10 20:22:02 -0400

    fix: install pkl first

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,5 +3,5 @@
 package version
 
 // Version holds the current version of the agent.
-// The default value is "0.0.1-alpha.1" and should be overridden at build time.
+// This value is generated at build time.
 var Version = "v1.0.0-alpha.1"
